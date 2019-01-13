f075e6470105b0b66298fbe66e77f0ec9b5d8836
Daniel <daniel.mengistu@hotmail.com>
2019-01-13 21:10:07 +0200

    redux inspector fixed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import Locations from "./components/Locations";
-import Header from "./components/Header"
+import StationMapContainer from "./components/StationMapContainer";
+import Header from "./components/Header";
 import "./App.css";
-import { Provider } from "react-redux"
-import store from "./store"
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-      <BrowserRouter>
-        <div className="App">
-          <Header/>
-          <Route exact path="/stations" component={Locations} />
-        </div>
+        <BrowserRouter>
+          <div className="App">
+            <Header />
+            <Route exact path="/stations" component={StationMapContainer} />
+          </div>
         </BrowserRouter>
       </Provider>
     );
--- src/components/StationMapContainer.js
+++ src/components/StationMapContainer.js
@@ -1,16 +1,44 @@
 import React, { Component } from "react";
 import StationsMap from "./StationsMap";
+import Loading from "react-loading-bar";
+import "react-loading-bar/dist/index.css";
+import { connect } from "react-redux";
+import { featchLocation } from "../actions/feachStationAction";
 
-export default class StationMapContainer extends Component {
+class StationMapContainer extends Component {
+  componentDidMount() {
+    this.props.featchLocation();
+  }
+
   render() {
+    const { loading, stations } = this.props;
+
     return (
-      <StationsMap
-        stations={this.props.stations}
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyBCHOXJzvHtr41MG_9k9-4RRUIK7lZLrvQ&v=3.exp&libraries=geometry,drawing,places`}
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `600px`, width: `600px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
+      <div>
+        <Loading show={loading} color="red" />
+        <StationsMap
+          stations={stations}
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyB308OAwh8rv0c1nH07VCJWJvTv4t2uOXk&v=3.exp&libraries=geometry,drawing,places`}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `600px`, width: `100%` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  loading: state.stations.loading,
+  stations: state.stations.stations,
+  error: state.stations.error
+});
+
+const mapActionToProps = {
+  featchLocation
+};
+
+export default connect(
+  mapStateToProps,
+  mapActionToProps
+)(StationMapContainer);
